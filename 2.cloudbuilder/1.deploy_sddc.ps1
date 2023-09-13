073770ecd1f17af8a0935f357191ec5fba7063b7
Kim Johansson <maxiepax@gmail.com>
2023-09-13 15:40:08 +0200

    tidied up 2.sddc

--- 2.cloudbuilder/1.deploy_sddc.ps1
+++
@@ -1,2 +0,0 @@
-Connect-CloudBuilder -fqdn sfo-cb01.sfo.rainpole.io -username admin -password VMware1!VMware1!
-Start-CloudBuilderSDDC -json ./vcf-ems.json
--- 2.cloudbuilder/1.deploy_sddc.sh
+++
@@ -1,4 +0,0 @@
-curl 'https://sfo-cb01.rainpole.local/v1/sddcs' -i -u 'admin:VMware1!' -X POST \
-    -H 'Content-Type: application/json' \
-    -H 'Accept: application/json' \
-    -d @vcf-ems.json
